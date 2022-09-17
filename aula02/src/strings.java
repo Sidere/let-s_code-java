74c9fa49e26f20182bf44e5568f395056eeaf942
Poliana Sidere <poli360-pp@hotmail.com>
2022-09-17 15:42:05 -0300

    first commit

---
+++ aula02/src/strings.java
@@ -0,0 +1,11 @@
+public class strings {
+    public static void main(String[] args) {
+        String nome;
+        nome = "Sidere";
+        nome = "Let's Code";
+        System.out.println("Olá, " + nome);
+
+
+
+    }
+}
